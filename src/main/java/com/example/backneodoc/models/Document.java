136963a877a06d02f756992eb9faedabab6a292a
Malak Zangar <64647127+malak-zangar@users.noreply.github.com>
2024-09-12 23:26:49 +0100

    Update Document.java

--- src/main/java/com/example/backneodoc/models/Document.java
+++ src/main/java/com/example/backneodoc/models/Document.java
@@ -37,9 +37,7 @@ public class Document {
     @JoinTable(name = "doc_tag", joinColumns = { @JoinColumn(name = "doc_id") }, inverseJoinColumns = { @JoinColumn(name = "tag_id") })
     private Set<Tag> tags = new HashSet<>();
 
-    /*@ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(name = "doc_dep", joinColumns = { @JoinColumn(name = "id_doc") }, inverseJoinColumns = { @JoinColumn(name = "id_dep") })
-    private Set<Departement> departements = new HashSet<>();*/
+
 
     @Column(name = "departement", nullable = false)
     private String departements ;
@@ -47,11 +45,6 @@ public class Document {
     public Document() {
     }
 
-   /* public Document(String titre, String type , byte[] data) {
-        this.titre = titre;
-        this.type=type;
-        this.data=data;
-    }*/
 
     public Document(String name, Long size, String contentType, String owner, String path, Set<Tag> tags, String departements) {
         this.name = name;
@@ -76,22 +69,6 @@ public class Document {
 
 
 
-    /* public Document(String titre, String type, byte[] data, Set<Tag> tags,String dep) {
-        this.titre = titre;
-        this.type = type;
-        this.data=data;
-        this.tags = tags;
-        this.departements=dep;
-    }*/
-
-    /*public Document(String titre, String type, byte[] data, Set<Tag> tags,String dep,String owner) {
-        this.titre = titre;
-        this.type = type;
-        this.data=data;
-        this.tags = tags;
-        this.departements=dep;
-        this.owner=owner;
-    }*/
 
     public Long getId() {return id;}
 
@@ -118,10 +95,6 @@ public class Document {
 
     public void setTags(Set<Tag> tags) {this.tags = tags;}
 
-   /* public byte[] getData() {return data;}
-
-    public void setData(byte[] data) {this.data = data;}
-*/
 
     public String getName() {
         return name;
@@ -171,15 +144,5 @@ public class Document {
         this.owner = owner;
     }
 
-    // public Set<Departement> getDepartements() {return departements;}
 
-   // public void setDepartements(Set<Departement> departements) {this.departements = departements;}
-/*
-    public Set<User> getFavoris() {
-        return favoris;
-    }
-
-    public void setFavoris(Set<User> favoris) {
-        this.favoris = favoris;
-    }*/
 }
